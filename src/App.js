f695f8b0295afa72085fc2376b7b587c049f0e35
Breno <breno.rosado@estudante.ufjf.br>
2023-10-24 21:51:02 -0300

    Edita os arquivos para fazer parte do jogo Idle clicker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cabecalho from "./components/Cabecalho/Cabecalho.js";
+import Recursos from "./components/Recursos/Recursos.js";
+import Acoes from "./components/Acoes/Acoes.js";
+import Loja from "./components/Loja/Loja.js";
+import Status from "./components/Status/Status.js";
+import Rodape from "./components/Rodape/Rodape.js";
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [estoque, setEstoque] = useState({
+    madeira: 0,
+    ouro: 0,
+    casa: 0,
+    trabalhador: 0,
+    comercio: 0,
+  });
+
+  const [transacoes, setTransacoes] = useState({
+    cortar: { madeira: 1 },
+    vender: { madeira: -1, ouro: 1 },
+    construir: { madeira: -5, ouro: -5, casa: +1 },
+    contratarLenhador: { ouro: -10, casa: -1, trabalhador: 1 },
+  });
+
+  const [processos, setProcessos] = useState({
+    cortar: 1,
+    vender: 1,
+  });
+
+  const handleClick = (e) => {
+    const id = e.target.id;
+    switch (id) {
+      case "cortarBtn":
+        setEstoque((prevState) => ({
+          ...prevState,
+          madeira: prevState.madeira + transacoes.cortar.madeira,
+        }));
+        break;
+      case "venderBtn":
+        if (estoque.madeira >= transacoes.vender.madeira * -1) {
+          setEstoque((prevState) => ({
+            ...prevState,
+            madeira: prevState.madeira + transacoes.vender.madeira,
+            ouro: prevState.ouro + transacoes.vender.ouro,
+          }));
+        }
+        break;
+      case "construirBtn":
+        if (
+          estoque.madeira >= transacoes.construir.madeira * -1 &&
+          estoque.ouro >= transacoes.construir.ouro * -1
+        ) {
+          setEstoque((prevState) => ({
+            ...prevState,
+            madeira: prevState.madeira + transacoes.construir.madeira,
+            ouro: prevState.ouro + transacoes.construir.ouro,
+            casa: prevState.casa + transacoes.construir.casa,
+          }));
+        }
+        break;
+      case "contratarLenhadorBtn":
+        if (
+          estoque.ouro >= transacoes.contratarLenhador.ouro * -1 &&
+          estoque.casa >= transacoes.contratarLenhador.casa * -1
+        ) {
+          setEstoque((prevState) => ({
+            ...prevState,
+            ouro: prevState.ouro + transacoes.contratarLenhador.ouro,
+            casa: prevState.casa + transacoes.contratarLenhador.casa,
+            trabalhador:
+              prevState.trabalhador + transacoes.contratarLenhador.trabalhador,
+          }));
+        }
+        break;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cabecalho />
+      <Recursos estoque={estoque} />
+      <Acoes transacoes={transacoes} handleClick={handleClick} />
+      <Rodape estoque={estoque} transacoes={transacoes} processos={processos} />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Rodape/Rodape.js
@@ -0,0 +1,16 @@
+import "./Rodape.css";
+import Loja from "../Loja/Loja.js";
+import Status from "../Status/Status.js";
+
+export default function Rodape(props) {
+  return (
+    <footer className="Rodape">
+      <Loja />
+      <Status
+        estoque={props.estoque}
+        transacoes={props.transacoes}
+        processos={props.processos}
+      />
+    </footer>
+  );
+}
